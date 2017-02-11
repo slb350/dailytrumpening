b479035b77a372f254312d76e6089ff1751d7b0a
Jeff Glenn <jeff@signaltrace.net>
2017-02-11 13:35:16 -0500

    Babel and other buzzwordy build stuff

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,8 @@ var sass = require('gulp-sass');
 var uglify = require('gulp-uglify');
 var cssmin = require('gulp-minify-css');
 var concat = require('gulp-concat');
+var babel = require('gulp-babel');
+var sourcemaps = require('gulp-sourcemaps');
 
 gulp.task('styles', function () {
     gulp.src('./assets/stylesheets/sass/index.scss')
@@ -12,10 +14,20 @@ gulp.task('styles', function () {
 });
 
 gulp.task('scripts', function() {
-    gulp.src(['./assets/js/src/**/*.js'])
-        .pipe(concat('app.min.js'))
+    gulp.src(['./assets/js/src/vendor/**/*.js'])
+        .pipe(concat('vendor.min.js'))
+        .pipe(sourcemaps.write('.'))
         .pipe(uglify())
-        .pipe(gulp.dest('assets/js'))
+        .pipe(gulp.dest('assets/js'));
+
+    gulp.src(['./assets/js/src/*.js'])
+        .pipe(babel({
+            presets: ['es2015']
+        }))
+        .pipe(concat('app.min.js'))
+        .pipe(sourcemaps.write('.'))
+        .pipe(uglify())
+        .pipe(gulp.dest('assets/js'));
 })
 
 gulp.task('default', function() {
