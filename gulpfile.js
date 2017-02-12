639120e12a3747a32f8e44efab49355f3ee34dc5
Jeff Glenn <jeff@signaltrace.net>
2017-02-12 17:00:02 -0500

    Sourcemaps and watch task

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,7 @@ var cssmin = require('gulp-minify-css');
 var concat = require('gulp-concat');
 var babel = require('gulp-babel');
 var sourcemaps = require('gulp-sourcemaps');
+var rev = require('gulp-rev');
 
 gulp.task('styles', function () {
     gulp.src('./assets/stylesheets/sass/index.scss')
@@ -15,21 +16,27 @@ gulp.task('styles', function () {
 
 gulp.task('scripts', function() {
     gulp.src(['./assets/js/src/vendor/**/*.js'])
+        .pipe(sourcemaps.init('.'))
         .pipe(concat('vendor.min.js'))
-        .pipe(sourcemaps.write('.'))
         .pipe(uglify())
+        .pipe(sourcemaps.write('.'))
         .pipe(gulp.dest('assets/js'));
 
     gulp.src(['./assets/js/src/*.js'])
+        .pipe(sourcemaps.init('.'))
         .pipe(babel({
             presets: ['es2015']
         }))
         .pipe(concat('app.min.js'))
-        .pipe(sourcemaps.write('.'))
         .pipe(uglify())
+        .pipe(sourcemaps.write('.'))
         .pipe(gulp.dest('assets/js'));
 })
 
 gulp.task('default', function() {
     gulp.run('styles', 'scripts');
+})
+
+gulp.task('watch', function(){
+    gulp.watch(['assets/js/src/**/*.js', 'assets/stylesheets/sass/**/*.scss'], ['default']);
 })
